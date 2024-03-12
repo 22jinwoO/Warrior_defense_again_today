c6dafaeb0cc9e07ea57b47fdd48eae821d66727b
이진우 <jinwoo2511@naver.com>
2024-03-12 22:07:47 +0900

    Fix : 위협적인 포효 스킬 기능 수정
    위협적인 포효 스킬 몬스터 5마리까지만 어그로 끌리도록 수정

--- 3D_Casual_Defense/Assets/1.Scripts/Skills/Unit_Skills/MenacingRoar.cs
+++ 3D_Casual_Defense/Assets/1.Scripts/Skills/Unit_Skills/MenacingRoar.cs
@@ -18,21 +18,26 @@ public class MenacingRoar : SpecialSkill
     }
     public override void Attack_Skill()
     {
-
+        int cnt = 0;
         StartCoroutine(VfxOnOFF());
         Collider[] colls = Physics.OverlapSphere(unitInfoCs.transform.position, 10f, unitTargetSearchCs._layerMask);
         print("B눌림");
-        for (int i = 0; i < colls.Length; i++)
+        if(cnt<5)
         {
-            UnitInfo unitInfo = colls[i].GetComponent<UnitInfo>();
-            print(colls[i].name);
-            unitInfo.unitTargetSearchCs._targetUnit = unitInfoCs.transform;
-            unitInfo.unitTargetSearchCs._target_Body = unitInfoCs.body_Tr;
-            //unitInfo.unitTargetSearchCs._target_Body = this.transform;
+            for (int i = 0; i < colls.Length; i++)
+            {
+                UnitInfo unitInfo = colls[i].GetComponent<UnitInfo>();
+                print(colls[i].name);
+                unitInfo._nav.ResetPath();
+                unitInfo.unitTargetSearchCs._targetUnit = unitInfoCs.transform;
+                unitInfo.unitTargetSearchCs._target_Body = unitInfoCs.body_Tr;
+                //unitInfo.unitTargetSearchCs._target_Body = this.transform;
 
-            unitInfo._enum_Unit_Action_Mode = eUnit_Action_States.monster_AngryPhase;
-            unitInfo._enum_Unit_Action_State = eUnit_Action_States.unit_Attack;
-            //unitInfo.unitTargetSearchCs._target_Body = 
+                unitInfo._enum_Unit_Action_Mode = eUnit_Action_States.monster_AngryPhase;
+                unitInfo._enum_Unit_Action_State = eUnit_Action_States.unit_Attack;
+                cnt++;
+                //unitInfo.unitTargetSearchCs._target_Body = 
+            }
 
         }
 
