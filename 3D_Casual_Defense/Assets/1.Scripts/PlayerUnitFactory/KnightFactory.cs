2c2f8263100eb473b4478bbda6ce860c1651e2dd
이진우 <jinwoo2511@naver.com>
2024-03-27 19:48:01 +0900

    Fix : 팩토리에서 데이터 할당하는 부분 삭제
    팩토리에서 데이터 할당하는 부분 삭제

--- 3D_Casual_Defense/Assets/1.Scripts/PlayerUnitFactory/KnightFactory.cs
+++ 3D_Casual_Defense/Assets/1.Scripts/PlayerUnitFactory/KnightFactory.cs
@@ -2,16 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class KnightFactory : AbsPlayerUnitFactory
 {
     // 기사 프리팹
     public Knight knightPrefab;
-
-    //private void Awake()
-    //{
-    //    // 기사 유닛 ID에 해당하는 제이슨 파일 찾아서 필요한 데이터 할당
-    //    // knightPrefab.InitUnitInfoSetting(UnitDataManager.Instance._unitInfo_Dictionary["hum_warr01"]);
-    //}
 
 
     // 기사 클래스마다 생산될 유닛을 결정해주는 구상 생산자
