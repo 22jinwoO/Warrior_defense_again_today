7aeeb2b2b5e89801c0b5ce8e95ed54dbac7086ba
이진우 <jinwoo2511@naver.com>
2024-03-27 18:14:59 +0900

    Fix : 빌보드 기능 수정
    빌보드 기능 수정

--- 3D_Casual_Defense/Assets/1.Scripts/BillBoard.cs
+++ 3D_Casual_Defense/Assets/1.Scripts/BillBoard.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BillBoard : MonoBehaviour
 {
-    Camera mainCamera;
+    [SerializeField]
+    private Camera mainCamera;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        // 메인카메라 가져오기
         mainCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 
     }
@@ -16,9 +20,7 @@ public class BillBoard : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // 오브젝트의 정면은 카메라의 정면과 같은 방향을 바라봄
         transform.forward = mainCamera.transform.forward;
-
-        //transform.LookAt(mainCamera.transform);
-        //transform.Rotate(transform.eulerAngles.x, 0f, 180);
     }
 }
